0b7eacfb378a2de1616a7802e091b4d5ff2bc9f9
mms <marcosms@gmail.com>
2024-01-22 21:38:49 +0100

    Hago nuevas pruebas modificando, recuperanado y guardando datos.

--- src/main/java/com/mms/starterMongoDB/UserController.java
+++ src/main/java/com/mms/starterMongoDB/UserController.java
@@ -3,6 +3,9 @@ package com.mms.starterMongoDB;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+import java.util.UUID;
+
 @RestController
 public class UserController {
 
@@ -22,5 +25,40 @@ public class UserController {
 
 		return "hola";
 	}
+
+	@GetMapping("/altaUser")
+	public String altaUser(){
+
+		User user = new User();
+		user.setCod(UUID.randomUUID().toString());
+		user.setName(UUID.randomUUID().toString());
+		user.setEdad(18);
+
+		User userSaved = userRepository.save(user);
+
+		System.out.println("userSaved: " + userSaved.toString());
+		return userSaved.toString();
+	}
+
+	@GetMapping("/modificarUser")
+	public String modificarUser(){
+
+		User userSaved = null;
+		//User userFound = userRepository.findById("22").orElse(null);
+		User userFound = userRepository.findByCod("2");
+
+		if (userFound != null){
+			userFound.setName( userFound.getName().concat("_modified"));
+
+			userSaved = userRepository.save(userFound);
+			System.out.println("userSaved: " + userSaved.toString());
+
+			return userSaved.toString();
+		}else{
+			return "no se ha econtrado al usuario";
+		}
+
+	}
+
 
 }
--- src/main/java/com/mms/starterMongoDB/UserRepository.java
+++ src/main/java/com/mms/starterMongoDB/UserRepository.java
@@ -3,4 +3,5 @@ package com.mms.starterMongoDB;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface UserRepository extends MongoRepository<User, String> {
+    User findByCod(String cod);
 }
